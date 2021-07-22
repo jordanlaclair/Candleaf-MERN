95c218c36431e8d57889222b7fe076a6536aecbd
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-07-21 21:04:15 -0500

    server side code

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "31mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "31mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+mongoose
+	.connect(process.env.CONNECTION_URL, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then(() => {
+		//if made it here, connected to database
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.error(err.message);
+	});
+
+mongoose.set("useFindAndModify", false);
