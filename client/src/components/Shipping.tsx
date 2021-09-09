657fe06033b1b4cd5c8c12a7c2d96807b6b9c7f8
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-09-09 12:03:08 -0500

    link to site

--- client/src/components/Shipping.tsx
+++ client/src/components/Shipping.tsx
@@ -407,7 +407,7 @@ const ShippingWrapper = styled(CheckoutWrapper)`
 const ShippingSecondHalf = styled.div`
 	background: ${(props) => props.theme.colors.secondary};
 	display: flex;
-	height: 110vh;
+	height: 100vh;
 	flex-direction: column;
 	padding: 2rem 0;
 	justify-content: center;
